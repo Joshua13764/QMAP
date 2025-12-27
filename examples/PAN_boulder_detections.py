5711d0d614af3ac081a4d95fb80144da005fdc31
Josh Briggs <joshua13764@Outlook.com>
2025-12-27 18:00:13 +0000

    Added PAN to LOD with supersampling

---
+++ examples/PAN_boulder_detections.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from typing import Any, List
+
+from boulder_statistics.environment_tools.fs_environment import FSEnvironment
+from boulder_statistics.environment_tools.fs_markers.fs_marker_string import \
+    FSMarkerString
+from boulder_statistics.file_storage_adapters.numpy_adapter import \
+    FSNumpyAdapter
+from boulder_statistics.result_cache import ResultCache
+from boulder_statistics.steps.PAN_to_LOD import PANToLOD
+from boulder_statistics.steps.PAN_to_LOD_supersample import PANToLODSuperSample
+from boulder_statistics.steps.simple_request import SimpleRequest
+from boulder_statistics.steps_orchestrator import StepsOrchestrator
+
+detections_from_bennu_pan: Path = Path(
+    r"C:\Users\Joshu\Documents\AO33_DATA\detections_from_bennu_pan")
+
+get_pan = SimpleRequest(
+    task_name=f"Downloader for the bennu PAN file",
+    url="https://svs.gsfc.nasa.gov/vis/a000000/a005000/a005069/Bennu_global_FB34_FB56_ShapeV28_GndControl_MinnaertPhase30_PAN_8bit.tif",
+    fs_path=detections_from_bennu_pan.as_posix(),
+    sub_path=Path("OCAMS", "Global PAN Mosaic.tif").as_posix(),
+    markers=(FSMarkerString(value="PAN_texture"),)
+)
+
+super_sample_steps = []
+super_sample_factors: list[int] = [1, 2, 4, 8]
+for factor in super_sample_factors:
+    super_sample_steps.append(
+        PANToLODSuperSample(
+            task_name=f"Convert bennu PAN to LODs - Numpy version (super sample x{factor})",
+            root_path=detections_from_bennu_pan,
+            run_after_task_names=(get_pan.task_name,),
+            supersample_factor=factor,
+            lod_res=512,
+            skip_if_exists=True,
+            import_markers=(FSMarkerString(value="PAN_texture"),),
+            export_markers=(
+                FSMarkerString(
+                    value=f"PAN_lod_np (super sample x{factor})"),),
+            extract_folder_prefix=f"PAN_lod_np (super sample x{factor})",
+            lod_depth=5,
+            export_adapter=FSNumpyAdapter(
+                export_debug_plots=True,
+                title=f"PAN to LOD NP with super sample x{factor}",
+                colour_bar_title="Pixel Value",
+            )
+        ))
+
+step8 = PDS4BoulderNetInference(
+    task_name=f"Infer boulders",
+    cuda=True,
+    skip_converted=True,
+    run_after_task_names=(*[step.task_name for step in super_sample_steps]),
+    run_path=pipeline_working_path_fast.as_posix(),
+    detection_output_markers=(FSMarkerString("BoulderNet_Detections"),)
+)
+
+steps: List[Any] = [get_pan, pan_to_lod_np, *super_sample_steps]
+
+if __name__ == "__main__":
+    cache: ResultCache[FSEnvironment] = ResultCache[FSEnvironment](
+        cache_folder=Path(".cache"), result_type=FSEnvironment)
+
+    futures: dict[str, FSEnvironment] = StepsOrchestrator.run_steps(
+        steps, cache)
