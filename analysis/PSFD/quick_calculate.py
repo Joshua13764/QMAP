0757a2a074cc0a3f1f8b2a960886771dbdd1db0f
Josh Briggs <joshua13764@Outlook.com>
2026-06-22 10:53:40 +0100

    Fixed examples and modified analysis

---
+++ analysis/PSFD/quick_calculate.py
@@ -0,0 +1,130 @@
+from dataclasses import dataclass
+from functools import cached_property
+import numpy as np
+from polars import Expr, LazyFrame
+from res import *
+from scipy.interpolate import interp1d
+from scipy.stats import gaussian_kde
+from scipy import integrate
+from functools import partial
+from scipy.integrate import trapezoid
+
+relative_alpha: Expr = pl.col("alpha") / (2 ** (2*4 - 2 * pl.col("tile_lod_number")))
+
+@dataclass
+class FittingFunction():
+    LAD_min : float = 2
+    max_fitting_alpha : float = 1e8 # 1e4
+    min_fitting_alpha : float = 1e1
+
+    @property
+    def a_min(self) -> np.float32:
+        return np.pi * (self.LAD_min) ** 2
+    
+    @property
+    def a_max(self) -> np.float32:
+        # return np.float32(self.cleaned_data.collect()["surface_area"].max()) * 1e6
+        return np.float32(self.cleaned_data.collect()["surface_area"].max()) * 1e6
+    
+    def sigma_sum(self, alphas, phis, phi_weights, q) -> np.ndarray:
+        return np.sum(np.where(
+            (self.a_min < alphas[:, None] / phis[None, :]) & (alphas[:, None] / phis[None, :] < self.a_max),
+            phi_weights[None, :] * (- q * phis[None, :] ** (q)) / (self.a_max ** (-q) - self.a_min  ** (-q)),
+            0
+        ), axis=1) / np.sum(phi_weights)
+    
+    @cached_property
+    def no_merge_sample(self) -> np.ndarray:
+        return combined_mask_no_merge.group_by("row_id").agg(
+                pl.len().alias("alpha"),
+                pl.col("lod_level").first().alias("tile_lod_number")
+            ).with_columns(
+                relative_alpha.alias("relative_alpha")
+            ).collect()["relative_alpha"].to_numpy()
+    
+    @cached_property
+    def sample(self) -> np.ndarray:
+        return boulder_agg_data.collect().with_columns(
+            relative_alpha.alias("relative_alpha")
+        )["relative_alpha"].to_numpy()
+
+    @cached_property
+    def S_fast_un_norm(self) -> interp1d:
+
+        log_sample = np.log(self.no_merge_sample[self.no_merge_sample > 5.5])
+        kde = gaussian_kde(log_sample)
+        
+        S_un_norm = lambda x : kde(np.log(x)) / x
+
+        xs_grid = np.geomspace(
+            self.no_merge_sample[self.no_merge_sample > 5.5].min(),
+            self.no_merge_sample.max(),
+            1000)
+
+        ys_grid = S_un_norm(xs_grid)
+
+        return interp1d(
+            xs_grid,
+            ys_grid,
+            kind="linear",
+            bounds_error=False,
+            fill_value=0.0
+        )
+    
+    def F(self, alphas, effectiveness, g, q) -> np.ndarray:
+        S_estimate = lambda x : self.S_fast_un_norm(x) / (x ** (-q))
+        total_p_alpha = (alphas ** (- q - 1)) * self.sigma_sum(
+            alphas = alphas,
+            phis = 0.1 * g * 0.5 * (Phi_counts_smoothed_bins[1:] + Phi_counts_smoothed_bins[:-1]),
+            phi_weights = Phi_counts_smoothed_counts,
+            q = q)
+
+        total_s = 1 - np.prod([
+            1 - effectiveness * 0.5 * S_estimate(alphas / (2 ** (2*4 - 2*i))) for i in range(5)
+        ], axis = 0)
+
+        p_estimate = total_p_alpha * total_s
+        p_estimate[alphas > self.max_fitting_alpha] = 0 # We don't fit larger than this as unreliable
+        p_estimate[alphas < self.min_fitting_alpha] = 0 # We don't fit smaller than this as unreliable
+
+        return p_estimate
+    
+    def int_F(self, effectiveness, g, q, int_samples = 40_000) -> np.floating:
+
+        int_alphas = np.geomspace(1, 1e6, int_samples)
+        int_probs = self.F(int_alphas, effectiveness = effectiveness, g = g, q = q)
+        finite_alphas = int_alphas[int_probs > 0]
+        finite_probs = int_probs[int_probs > 0]
+
+        return np.abs(trapezoid(finite_alphas, finite_probs))
+
+    def F_norm(self, alphas, effectiveness, g, q):
+
+        int_F = self.int_F(effectiveness, g, q)
+        return self.F(alphas, effectiveness, g = g, q = q) / int_F
+    
+    @property
+    def plot_range(self):
+        min = self.cleaned_data.collect()["alpha"].min()
+        max = self.cleaned_data.collect()["alpha"].max()
+
+        return min, max
+    
+    @property
+    def cleaned_data(self) -> LazyFrame:
+        target = boulder_agg_data.filter(pl.col("longest_axis_diameter") * 1000 > self.LAD_min, pl.col("alpha") < 1e3).collect()
+        most = boulder_agg_data.filter(pl.col("longest_axis_diameter") * 1000 > self.LAD_min).collect()
+
+        most_ratios = most["longest_axis_diameter"].to_numpy() / most["surface_area"].to_numpy()
+        target_ratios = target["longest_axis_diameter"].to_numpy() / target["surface_area"].to_numpy()
+        most_ratios_clean = most_ratios[~np.isnan(most_ratios)]
+        target_ratios_clean = target_ratios[~np.isnan(target_ratios)]
+
+        mean_p2std = most_ratios_clean.mean() + 2 * most_ratios_clean.std()
+
+        return boulder_agg_data.filter(
+            pl.col("longest_axis_diameter") * 1000 > self.LAD_min,
+            (pl.col("longest_axis_diameter") / pl.col("surface_area")) < mean_p2std,
+            pl.col("alpha") < self.max_fitting_alpha, # We don't fit larger than this as unreliable
+            pl.col("alpha") > self.min_fitting_alpha, # We don't fit smaller than this as unreliable
+            )
--- examples/main_run.py
+++ examples/main_run.py
@@ -1,171 +1,167 @@
 from pathlib import Path
-from typing import List
+from typing import Any, List
 
 from boulder_statistics.environment_tools.fs_environment import FSEnvironment
+from boulder_statistics.environment_tools.fs_input import FSInput
 from boulder_statistics.environment_tools.fs_markers.fs_marker_string import \
     FSMarkerString
+from boulder_statistics.file_storage_adapters.bennu_obj_to_las_cubemap_generator_adapter import \
+    FSBennuObjToLODCubemapGeneratorAdapter
+from boulder_statistics.file_storage_adapters.fs_copy_cubemap_generator_adapter import \
+    FSCopyCubemapGeneratorAdapter
+from boulder_statistics.file_storage_adapters.fs_generic_cubemap_generator_adapter import \
+    FSGenericCubemapGeneratorAdapter
+from boulder_statistics.file_storage_adapters.iio_adapter import FSIIOAdapter
+from boulder_statistics.file_storage_adapters.inference_detection_adapter import \
+    FSInferenceDetectionAdapter
 from boulder_statistics.file_storage_adapters.numpy_adapter import \
     FSNumpyAdapter
+from boulder_statistics.file_storage_adapters.pan_to_lod_cubemap_generator_adapter import \
+    FSPANToLODCubemapGeneratorAdapter
+from boulder_statistics.file_storage_adapters.pickle_adapter import \
+    FSPickleAdapter
+from boulder_statistics.file_storage_adapters.polars_lazy_action_csv_batched_adapter import \
+    FSPolarsLazyActionBatched
+from boulder_statistics.file_storage_adapters.polars_lazy_csv_adapter import \
+    FSPolarsLazyCSVAdapter
+from boulder_statistics.file_storage_adapters.polars_obj_adapter_fast_PL_obj_data import \
+    FSPolarsObjAdapterFastPLOBJData
+from boulder_statistics.file_storage_adapters.type_safe_pickle_adapter import \
+    FSTypeSafePickleAdapter
+from boulder_statistics.lods.utils.image_detection_grades import \
+    ImageDetectionGrades
 from boulder_statistics.result_cache import ResultCache
-from boulder_statistics.step_base import StepBase
-from boulder_statistics.steps.Best_model_downloader import BestModelDownloader
-from boulder_statistics.steps.detection_merge import DetectionMerge
-from boulder_statistics.steps.OBJ_to_LAS import OBJToLAS
-from boulder_statistics.steps.PAN_to_LOD import PANToLOD
-from boulder_statistics.steps.pds4_boulderNet_inference import \
-    PDS4BoulderNetInference
-from boulder_statistics.steps.PDS_downloader import PDSDownloader
-from boulder_statistics.steps.PDS_to_PNG import PDS_to_PNG
-from boulder_statistics.steps.plot_standard_detection_results import \
-    PlotStandardDetectionResults
+from boulder_statistics.steps.Better_OBJ_to_LAS import BetterOBJToLAS
+from boulder_statistics.steps.Better_PAN_to_LOD import BetterPANToLOD
+from boulder_statistics.steps.better_PDS4_boulder_net_inference import \
+    BetterPDS4BoulderNetInference
+from boulder_statistics.steps.export_full_data_pack import ExportFullDataPack
+from boulder_statistics.steps.setup_boulder_net_inferences_for_grading import \
+    SetupBoulderNetInferencesForGrading
 from boulder_statistics.steps.simple_request import SimpleRequest
 from boulder_statistics.steps_orchestrator import StepsOrchestrator
 
-model_download_path: Path = Path(r"F:\AO33\AO33_models")
-pds_download_path: Path = Path(r"F:\AO33\AO33_pds_DATA")
-pipeline_working_path: Path = Path(r"F:\AO33\AO33_pipeline_DATA")
-pipeline_working_path_fast: Path = Path(
-    r"C:\Users\Joshu\Documents\AO33_DATA\Pipeline_running_path_fast")
-boulderNet_preprocessor_test_folder: Path = Path(
-    r"C:\Users\Joshu\Documents\AO33_DATA\BoulderNetPreProcessorTests"
-)
-spice_download_path: Path = Path(r"F:\AO33\AO33_SPICE_DATA")
+detections_from_bennu_pan: Path = Path(
+    r"G:\AO33_pipeline_folders\LAS export + 30 detect threshold + D4_transforms")
 
-step1 = BestModelDownloader(
-    task_name="Download the best boulderNet model",
-    DownloadPath=model_download_path.as_posix(),
-    Url="https://zenodo.org/records/8171052/files/best_model.zip?download=1"
-)
-
-step2 = SimpleRequest(
-    task_name=f"Downloader for the bennu PAN file",
+get_pan = SimpleRequest(
+    task_name=f"Downloader for the bennu PAN",
     url="https://svs.gsfc.nasa.gov/vis/a000000/a005000/a005069/Bennu_global_FB34_FB56_ShapeV28_GndControl_MinnaertPhase30_PAN_8bit.tif",
-    fs_path=pds_download_path.as_posix(),
+    fs_path=detections_from_bennu_pan.as_posix(),
     sub_path=Path("OCAMS", "Global PAN Mosaic.tif").as_posix(),
     markers=(FSMarkerString(value="PAN_texture"),)
 )
 
-step3 = SimpleRequest(
+get_bennu_obj = SimpleRequest(
     task_name=f"Downloader for the bennu OBJ (LQ) mesh",
     url="https://svs.gsfc.nasa.gov/vis/a000000/a005000/a005069/g_00880mm_alt_ptm_0000n00000_v020.obj",
-    fs_path=pds_download_path.as_posix(),
+    fs_path=detections_from_bennu_pan.as_posix(),
     sub_path=Path(
         "OCAMS",
         "Global Bennu 3D model - OLA v20 PTM.obj").as_posix(),
     markers=(
         FSMarkerString(
-            value="OCAMS Model"),
-        FSMarkerString("ProjectModel"))
+            value="OCAMS_Model"),)
 )
 
-steps4: List[PDSDownloader] = [PDSDownloader(
-    task_name=f"Downloader for PDS file {url}",
-    DownloadPath=pds_download_path.as_posix(),
-    Url=url
-)
-    for url in [
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_data_calibrated_detailed_survey.zip",
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_data_reduced_detailed_survey.zip",
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_data_calibrated_orbit_b.zip",
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_data_calibrated_recon.zip",
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_metadata.zip",
-    "https://sbnarchive.psi.edu/pds4/orex/downloads_ocams/ocams_calibration.zip"]
-]
+lod_export_adapter: FSPANToLODCubemapGeneratorAdapter = FSPANToLODCubemapGeneratorAdapter(
+    tiles_adapter=FSNumpyAdapter(export_debug_plots=True, title="Export lod", colour_bar_title="colour value"), n_jobs=4)
 
-step5 = PDS_to_PNG(
-    task_name=f"Convert cluster ocams_data_calibrated_detailed_survey",
-    run_after_task_names=(steps4[0].task_name,),
-    cluster_key="ocams_data_calibrated_detailed_survey",
-    run_path=pipeline_working_path.as_posix()
+divide_pan: BetterPANToLOD = BetterPANToLOD(
+    task_name="divide pan into lods",
+    lod_depth=4,
+    run_after_task_names=(get_pan.task_name,),
+    input_adapter=FSIIOAdapter(),
+    output_adapter=lod_export_adapter,
+    input_markers=(FSMarkerString(value="PAN_texture"),),
+    output_markers=(FSMarkerString(value="PAN_lod"),),
+    pipeline_data_path=detections_from_bennu_pan,
+    n_jobs=1,
 )
 
-step6 = PANToLOD(
-    task_name=f"Convert bennu PAN to LODs",
-    root_path=pipeline_working_path_fast,
-    run_after_task_names=(step2.task_name,),
-    extract_folder_prefix="PAN_lod_default",
-    lod_res=512,
-    lod_depth=6,
-    skip_if_exists=True
+las_export_adapter: FSBennuObjToLODCubemapGeneratorAdapter = FSBennuObjToLODCubemapGeneratorAdapter(
+    tiles_adapter=FSNumpyAdapter(export_debug_plots=True, title="Export LAS", colour_bar_title="1 / LAS factor", transform=lambda x: 1 / x))
+
+export_las = BetterOBJToLAS(
+    task_name="divide las into lods",
+    lod_depth=4,
+    run_after_task_names=(get_bennu_obj.task_name,),
+    input_adapter=FSPolarsObjAdapterFastPLOBJData(),
+    output_adapter=las_export_adapter,
+    input_markers=(FSMarkerString(value="OCAMS_Model"),),
+    output_markers=(FSMarkerString(value="LAS_lod"),),
+    pipeline_data_path=detections_from_bennu_pan,
+    n_jobs=1,
 )
 
-pan_to_lod_np = PANToLOD(
-    task_name=f"Convert bennu PAN to LODs - Numpy version",
-    root_path=pipeline_working_path_fast,
-    run_after_task_names=(step2.task_name,),
-    lod_res=512,
-    skip_if_exists=True,
-    import_markers=(FSMarkerString(value="PAN_texture"),),
-    export_markers=(FSMarkerString(value="PAN_lod_np"),),
-    extract_folder_prefix="PAN_lod_np",
-    lod_depth=6,
-    export_adapter=FSNumpyAdapter()
-)
-
-step7 = OBJToLAS(
-    task_name=f"Convert bennu Mesh to stretch maps",
-    run_after_task_names=(step3.task_name,),
-    lod_res=512,
-    export_folder=pipeline_working_path_fast.as_posix(),
-    depth=6,
-    skip_if_exists=True,
-    debug_mode=False
-)
-
-step8 = PDS4BoulderNetInference(
-    task_name=f"Infer boulders",
+detection = BetterPDS4BoulderNetInference(
+    task_name=f"Infer boulders on bennu PAN LODs with BoulderNet",
+    run_after_task_names=(divide_pan.task_name,),
     cuda=True,
-    skip_converted=True,
-    run_after_task_names=(step6.task_name, step1.task_name,),
-    run_path=pipeline_working_path_fast.as_posix(),
-    detection_output_markers=(FSMarkerString("BoulderNet_Detections"),)
+    input_adapter=FSCopyCubemapGeneratorAdapter(),
+    input_markers=(FSMarkerString(value="PAN_lod"),),
+    output_markers=(FSMarkerString(value="INF_lod"),),
+    pipeline_data_path=detections_from_bennu_pan,
+    n_jobs=1,
 )
 
-step10 = DetectionMerge(
-    task_name=f"Merge detections",
-    run_after_task_names=(step8.task_name, step7.task_name),
-    marker_to_merge=FSMarkerString("BoulderNet_Detections"),
-    output_marker=FSMarkerString("Merged_BoulderNet_Detections"),
-    run_path=pipeline_working_path_fast.as_posix(),
-    result_output_path=Path("exports/merge_detections.pkl").as_posix()
+grades = SetupBoulderNetInferencesForGrading(
+    # debug_mode=True,
+    task_name=f"Setup inferences for grading",
+    run_after_task_names=(
+        detection.task_name,
+        divide_pan.task_name,
+        export_las.task_name),
+    pipeline_data_path=detections_from_bennu_pan,
+    output_markers=(FSMarkerString(value="INFCOLL_lod"),),
+    output_adapter=FSPickleAdapter(),
+    lod_images_input=FSInput(
+        fs_marker=FSMarkerString(value="PAN_lod"),
+        fs_adapter=FSGenericCubemapGeneratorAdapter(
+            tiles_adapter=FSNumpyAdapter())
+    ),
+    LAS_factor_input=FSInput(
+        fs_marker=FSMarkerString(value="LAS_lod"),
+        fs_adapter=FSGenericCubemapGeneratorAdapter(
+            tiles_adapter=FSNumpyAdapter())
+    ),
+    lod_detections_input=FSInput(
+        fs_marker=FSMarkerString(value="INF_lod"),
+        fs_adapter=FSGenericCubemapGeneratorAdapter(
+            tiles_adapter=FSInferenceDetectionAdapter())
+    ),
+    input_markers=None
 )
 
-step11 = PlotStandardDetectionResults(
-    task_name=f"Plot standard detection results",
-    run_after_task_names=(step10.task_name,),
-    marker_to_plot=FSMarkerString("Merged_BoulderNet_Detections"),
-    output_marker=FSMarkerString("Detection_Plots"),
-    export_folder=pipeline_working_path_fast.as_posix(),
-    result_output_folder=Path("exports/detection_plots").as_posix(),
-    version_index=3
+export_detections = ExportFullDataPack(
+    task_name=f"Export full data pack to DF",
+    run_after_task_names=(grades.task_name, ),
+    pipeline_data_path=detections_from_bennu_pan,
+    input_adapter=FSTypeSafePickleAdapter(expected_type=ImageDetectionGrades),
+    output_adapter=FSPolarsLazyActionBatched(
+        temp_folder_path=Path(
+            r"C:\Users\Joshu\Documents\AO33\temp\export_lazyframe").as_posix(),
+        n_jobs=4,
+        export_lazy_frame_adapter=FSPolarsLazyCSVAdapter(),
+        standard_extension="csv"),
+    input_markers=(FSMarkerString(value="INFCOLL_lod"),),
+    output_markers=(FSMarkerString(value="GRAD_lod_export"),),
 )
 
-steps = [
-    step1,
-    step2,
-    step3,
-    *steps4,
-    step5,
-    step6,
-    step7,
-    step8,
-    step10,
-    step11,
-    pan_to_lod_np]
 
-# step9 = SPICEKernelGrabber(
-#     task_name=f"Collect SPICE kernels",
-#     DownloadPath=spice_download_path.as_posix(),
-#     MkUrls=(
-#         "https://naif.jpl.nasa.gov/pub/naif/pds/pds4/orex/orex_spice/spice_kernels/mk/orx_2019_v08.tm",
-#     ),
-#     ExtraUrls=("https://naif.jpl.nasa.gov/pub/naif/pds/pds4/orex/orex_spice/spice_kernels/dsk/bennu_g_00880mm_alt_obj_0000n00000_v021a.bds",),
-# )
-
+steps: List[Any] = [
+    get_pan,
+    divide_pan,
+    detection,
+    grades,
+    export_detections,
+    export_las,
+    get_bennu_obj
+]
 
 if __name__ == "__main__":
     cache: ResultCache[FSEnvironment] = ResultCache[FSEnvironment](
         cache_folder=Path(".cache"), result_type=FSEnvironment)
 
-    futures: dict[str, FSEnvironment] = StepsOrchestrator.run_tasks_with_dependencies(
-        [step11], steps, cache)
+    futures: dict[str, FSEnvironment] = StepsOrchestrator.run_steps(
+        steps, cache)
